130cc567176570b7b30443c03b7a837e03a20a99
ihsansunman <ihsansunman@yandex.com>
2022-06-29 15:19:08 +0300

    Set asyncHandler

--- Backend/controllers/notController.js
+++ Backend/controllers/notController.js
@@ -1,28 +1,30 @@
-const getNotlar = (req, res) => {
-    res.status(200).json({ mesat: "get notlar" });
-}
+const asyncHandler = require("express-async-handler");
 
-const setNotlar = (req, res) => {
-    if(!req.body.mesaj){
-        //res.status(400).json({mesaj: 'Lütfen mesaj yazınız.'})
-        res.status(400)
-        throw new Error('Lütfen mesaj yazınız.')
-    }
+const getNotlar = asyncHandler(async (req, res) => {
+  res.status(200).json({ mesat: "get notlar" });
+});
 
-    res.status(200).json({ mesaj: "post not" });
-}
+const setNotlar = asyncHandler(async(req, res) => {
+  if (!req.body.mesaj) {
+    //res.status(400).json({mesaj: 'Lütfen mesaj yazınız.'})
+    res.status(400);
+    throw new Error("Lütfen mesaj yazınız.");
+  }
 
-const updateNotlar = (req, res) => {
-    res.status(200).json({ mesaj: `put ${req.params.id} idli not` });
-}
+  res.status(200).json({ mesaj: "post not" });
+})
 
-const deleteNotlar = (req, res) => {
-    res.status(200).json({ mesaj: `delete ${req.params.id} idli not` });
-}
+const updateNotlar = asyncHandler(async(req, res) => {
+  res.status(200).json({ mesaj: `put ${req.params.id} idli not` });
+})
 
-module.exports={
-    getNotlar,
-    setNotlar,
-    updateNotlar,
-    deleteNotlar
-}
+const deleteNotlar = asyncHandler(async(req, res) => {
+  res.status(200).json({ mesaj: `delete ${req.params.id} idli not` });
+})
+
+module.exports = {
+  getNotlar,
+  setNotlar,
+  updateNotlar,
+  deleteNotlar,
+};
