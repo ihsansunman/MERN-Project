f75ccf1c1cdcf11c3da6d818eafb1a97674f9e71
ihsansunman <ihsansunman@yandex.com>
2022-07-04 14:25:25 +0300

    Set Register

--- Backend/controllers/kullaniciController.js
+++ Backend/controllers/kullaniciController.js
@@ -1,6 +1,23 @@
-const registerKullanici = (req, res) => {
+const asyncHandler=require('express-async-handler')
+const Kullanici=require('../models/kullaniciModel')
+
+const registerKullanici = asyncHandler(async (req, res) => {
+  const {kullaniciAd, email,parola}=req.body;
+
+  if(!kullaniciAd || !email ||!parola){
+    res.status(400)
+    throw new Error('Gerekli alanları doldurulunuz.')
+  }
+
+  const kullanici=await Kullanici.findOne({email})
+
+  if(kullanici){
+    res.status(400)
+    throw new Error('Bu mail zaten sistemde kayıtlıdır.')
+  }
+
   res.json({ mesaj: "Kullanıcı Register İşlemleri" });
-};
+})
 
 const loginKullanici = (req, res) => {
   res.json({ mesaj: "Kullanici Login İşlemleri" });
