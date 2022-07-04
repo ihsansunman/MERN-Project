a9c42ed2e4683ebb49fe3cffa3f57786e6a2e008
ihsansunman <ihsansunman@yandex.com>
2022-07-04 14:52:39 +0300

    Set Password

--- Backend/controllers/kullaniciController.js
+++ Backend/controllers/kullaniciController.js
@@ -1,5 +1,6 @@
 const asyncHandler=require('express-async-handler')
 const Kullanici=require('../models/kullaniciModel')
+const bcrypt=require('bcryptjs')
 
 const registerKullanici = asyncHandler(async (req, res) => {
   const {kullaniciAd, email,parola}=req.body;
@@ -15,6 +16,27 @@ const registerKullanici = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('Bu mail zaten sistemde kayıtlıdır.')
   }
+
+  const salt=await bcrypt.genSalt(10)
+  const sifrelenmisParola= await bcrypt.hash(parola,salt)
+
+  const yeniKullanici=await Kullanici.create({
+    kullaniciAd,
+    email,
+    parola:sifrelenmisParola
+  })
+
+  if(yeniKullanici){
+    res.status(201).json({
+      _id:yeniKullanici.id,
+      kullaniciAd:yeniKullanici.kullaniciAd,
+      email:yeniKullanici.email
+    })}
+    else{
+      res.status(400)
+      throw new Error('Geçersiz kullanıcı verisi')
+    }
+  
 
   res.json({ mesaj: "Kullanıcı Register İşlemleri" });
 })
