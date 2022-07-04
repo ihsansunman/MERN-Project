43fc9d6ac3c2474f93c5eb11ca6198f41360498d
ihsansunman <ihsansunman@yandex.com>
2022-07-04 15:00:25 +0300

    Set user login action

--- Backend/controllers/kullaniciController.js
+++ Backend/controllers/kullaniciController.js
@@ -1,49 +1,62 @@
-const asyncHandler=require('express-async-handler')
-const Kullanici=require('../models/kullaniciModel')
-const bcrypt=require('bcryptjs')
+const asyncHandler = require("express-async-handler");
+const Kullanici = require("../models/kullaniciModel");
+const bcrypt = require("bcryptjs");
 
 const registerKullanici = asyncHandler(async (req, res) => {
-  const {kullaniciAd, email,parola}=req.body;
+  const { kullaniciAd, email, parola } = req.body;
 
-  if(!kullaniciAd || !email ||!parola){
-    res.status(400)
-    throw new Error('Gerekli alanları doldurulunuz.')
+  if (!kullaniciAd || !email || !parola) {
+    res.status(400);
+    throw new Error("Gerekli alanları doldurulunuz.");
   }
 
-  const kullanici=await Kullanici.findOne({email})
+  const kullanici = await Kullanici.findOne({ email });
 
-  if(kullanici){
-    res.status(400)
-    throw new Error('Bu mail zaten sistemde kayıtlıdır.')
+  if (kullanici) {
+    res.status(400);
+    throw new Error("Bu mail zaten sistemde kayıtlıdır.");
   }
 
-  const salt=await bcrypt.genSalt(10)
-  const sifrelenmisParola= await bcrypt.hash(parola,salt)
+  const salt = await bcrypt.genSalt(10);
+  const sifrelenmisParola = await bcrypt.hash(parola, salt);
 
-  const yeniKullanici=await Kullanici.create({
+  const yeniKullanici = await Kullanici.create({
     kullaniciAd,
     email,
-    parola:sifrelenmisParola
-  })
+    parola: sifrelenmisParola,
+  });
 
-  if(yeniKullanici){
+  if (yeniKullanici) {
     res.status(201).json({
-      _id:yeniKullanici.id,
-      kullaniciAd:yeniKullanici.kullaniciAd,
-      email:yeniKullanici.email
-    })}
-    else{
-      res.status(400)
-      throw new Error('Geçersiz kullanıcı verisi')
-    }
-  
+      _id: yeniKullanici.id,
+      kullaniciAd: yeniKullanici.kullaniciAd,
+      email: yeniKullanici.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Geçersiz kullanıcı verisi");
+  }
 
   res.json({ mesaj: "Kullanıcı Register İşlemleri" });
-})
+});
 
-const loginKullanici = (req, res) => {
+const loginKullanici = asyncHandler(async (req, res) => {
+  const { email, parola } = req.body;
+  const kullanici = await Kullanici.findOne({ email });
+
+  if (kullanici && (await bcrypt.compare(parola, kullanici.parola))) {
+    res.json({
+      __id: kullanici.id,
+      kullaniciAd: kullanici.kullaniciAd,
+      email: kullanici.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Geçersiz Email veya Parola");
+  }
+
   res.json({ mesaj: "Kullanici Login İşlemleri" });
-};
+});
 
 const getKullanici = (req, res) => {
   res.json({ mesaj: "Kullanıcı Get İşlemleri" });
