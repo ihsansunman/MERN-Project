627a9952d5c53fb5bb99a723e7cc99eee77a5e48
ihsansunman <ihsansunman@yandex.com>
2022-07-04 12:28:37 +0300

    Create DB Create

--- Backend/controllers/notController.js
+++ Backend/controllers/notController.js
@@ -1,26 +1,31 @@
 const asyncHandler = require("express-async-handler");
+const notModel = require("../models/notModel");
 
 const getNotlar = asyncHandler(async (req, res) => {
-  res.status(200).json({ mesat: "get notlar" });
+  res.status(200).json({ mesaj: "get notlar" });
 });
 
-const setNotlar = asyncHandler(async(req, res) => {
-  if (!req.body.mesaj) {
-    //res.status(400).json({mesaj: 'Lütfen mesaj yazınız.'})
+const setNotlar = asyncHandler(async (req, res) => {
+  if (!req.body.baslik || !req.body.aciklama) {
     res.status(400);
-    throw new Error("Lütfen mesaj yazınız.");
+    throw new Error("Lütfen başlık ve açıklama alanlarını giriniz");
   }
+  const not = await notModel.create({
+    baslik: req.body.baslik,
+    aciklama: req.body.aciklama,
+    oncelik: req.body.oncelik,
+  });
 
-  res.status(200).json({ mesaj: "post not" });
-})
+  res.status(200).json(not)
+});
 
-const updateNotlar = asyncHandler(async(req, res) => {
+const updateNotlar = asyncHandler(async (req, res) => {
   res.status(200).json({ mesaj: `put ${req.params.id} idli not` });
-})
+});
 
-const deleteNotlar = asyncHandler(async(req, res) => {
+const deleteNotlar = asyncHandler(async (req, res) => {
   res.status(200).json({ mesaj: `delete ${req.params.id} idli not` });
-})
+});
 
 module.exports = {
   getNotlar,
