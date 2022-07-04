6e24f555ef771347550d88b345a44aff5263c67d
ihsansunman <ihsansunman@yandex.com>
2022-07-04 11:18:34 +0300

    Connect MongoDB

---
+++ Backend/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const baglan = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Bağlandı--> ${conn.connection.name}`.blue);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+module.exports = baglan;
--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,19 @@
 const { request, response } = require("express");
 const express = require("express");
 const dotenv = require("dotenv").config();
-const {hataYakala}=require('./middlewares/errorMiddleware')
+const { hataYakala } = require("./middlewares/errorMiddleware");
+const baglan = require("./config/db");
+const colors = require("colors")
 
 const PORT = process.env.PORT;
 
 const app = express();
 
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use("/api/notlar", require("./routes/notRoute"));
-app.use(hataYakala)
+app.use(hataYakala);
+baglan();
 
-app.listen(PORT, () => console.log(`Server ${PORT} üzerinden yayında.`));
+app.listen(PORT, () => console.log(`Server ${PORT} üzerinden yayında.`.magenta.italic));
