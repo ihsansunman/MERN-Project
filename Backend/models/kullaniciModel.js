31fe79e8d2ee6fbbe63a290cbaba901c086bbb9f
ihsansunman <ihsansunman@yandex.com>
2022-07-04 13:06:41 +0300

    Create Users Model

---
+++ Backend/models/kullaniciModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const kullaniciSchema=mongoose.Schema({
+    kullaniciAd:{
+        type: String,
+        required: [true, "Kullanıcı adı belirleyiniz."]
+    },
+    email:{
+        type:String,
+        required:[true, "Lütfen E-Posta Belirleyiniz."],
+        unique: true
+    },
+    parola:{
+        type: String,
+        required:[true,"Lütfen Parola Belirleyiniz"]
+    }
+},{
+    timestramps:true
+})
+
+module.exports=mongoose.model('kullanici',kullaniciSchema)
